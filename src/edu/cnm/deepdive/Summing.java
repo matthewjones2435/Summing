6d563fd6efaf21c0f4230ce7c269e26509f753f2
matthewjones <matthewjones2435@gmail.com>
2019-05-20 16:21:33 -0600

    Initial commit of a summing application.

---
+++ src/edu/cnm/deepdive/Summing.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive;   // Matthew Jones, Brian Alexander, Dylon Vance
+
+public class Summing {
+
+  public static void main(String[] args) {
+    int sum = 0;
+
+    for (int i = 1; i <= 100; i++){
+
+      sum = sum + i;
+    }
+
+    System.out.println("The sum of 1 to 100 is " + sum);
+  }
+
+
+}
